a11f97ab4e5f424563cf31d65c5e56b2156a2e28
George Rios <george@plsft.com>
2026-04-11 15:12:14 -0400

    fix: cf deploy

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_D8TO1n-o.mjs';
-import { manifest } from './manifest_-SvSDTW6.mjs';
+import { manifest } from './manifest_CMBU8z6z.mjs';
 
 const serverIslandMap = new Map();;
 
